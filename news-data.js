ac47229ea97d9440e904fe435e1e04b88dcee56b
Hui Chen <51182630+HuiChen3313@users.noreply.github.com>
2026-09-02 15:21:17 +0700

    Update news-data.js

--- news-data.js
+++ news-data.js
@@ -1,1 +1,26 @@
+const newsData = [
 
+  {
+    year: "2026",
+    title: "Paper Accepted in IEEE Transactions on Affective Computing",
+    description:
+      'Our paper "TEMPO: Training-Time Equilibration of Modalities for Per-Sample Optimization in Multimodal Sentiment" has been accepted by IEEE Transactions on Affective Computing.'
+  },
+
+
+  {
+    year: "2026",
+    title: "Paper Accepted in Pattern Recognition",
+    description:
+      'Our paper "Exploring Personalized Federated Learning from a Distribution-Based Perspective" has been accepted by Pattern Recognition.'
+  },
+
+
+  {
+    year: "2026",
+    title: "Organizing SENTIRE Workshop at ICDM 2026",
+    description:
+      "We are involved in the organization of the SENTIRE workshop, an IEEE ICDM workshop series on sentiment analysis and affective computing."
+  }
+
+];
